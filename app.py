3741ca6c0ac34bfa822a359f2ac2c3027a559589
pctmoraes <paula.ct.moraes@gmail.com>
2023-10-27 18:36:50 -0300

    Code refactoring

--- app.py
+++ app.py
@@ -5,42 +5,46 @@ def create_app():
     app = Flask(__name__)
     app.debug = True
 
+    home_path = 'home.html'
+    card_form_path = 'card_form.html'
+    empty_wallet_path = 'empty_wallet.html'
+
     # Here would be the cards fetched from the DB
     card_list = list()
 
-    @app.route("/", methods=["GET", "POST"])
+    @app.route("/home", methods=["GET"])
     def home():
-        if request.method == "GET":
-            if card_list:
-                return render_template("home.html", card_list=card_list)
+        if card_list:
+            return render_template(home_path, card_list=card_list)
+        else:
+            return render_template(empty_wallet_path)
+    
+    @app.route("/card-form", methods=["GET"])
+    def card_form():
+        return render_template(card_form_path, card=None)
+   
+    @app.route("/add-card", methods=["POST"])
+    def add_card():
+        card = Card(
+            card_name = request.form.get('card_name'),
+            card_number = request.form.get('card_number'),
+            card_valid_thru = request.form.get('card_valid_thru'),
+            card_ccv = request.form.get('card_ccv'),
+            is_card_default = request.form.get('is_card_default')
+        )
+
+        if card.validation_messages:
+            return render_template(card_form_path, card=card)
+        else:
+            if card.is_card_default:
+                for c in card_list:
+                    if c.is_card_default:
+                        c.is_card_default = False
+                
+                card_list.insert(0,card)
             else:
-                return render_template("empty_wallet.html")
-        elif request.method == "POST":
-            form_class = request.form.get('form_class')
+                card_list.append(card)
 
-            if form_class == 'get_card_form':
-                return render_template("add_card.html", card=None)
-            else:
-                card = Card(
-                    card_name = request.form.get('card_name'),
-                    card_number = request.form.get('card_number'),
-                    card_valid_thru = request.form.get('card_valid_thru'),
-                    card_ccv = request.form.get('card_ccv'),
-                    is_card_default = request.form.get('is_card_default')
-                )
-
-                if card.validation_messages:
-                    return render_template("add_card.html", card=card)
-                else:
-                    if card.is_card_default:
-                        for c in card_list:
-                            if c.is_card_default:
-                                c.is_card_default = False
-                        
-                        card_list.insert(0,card)
-                    else:
-                        card_list.append(card)
-
-                    return render_template("home.html",card_list=card_list)
+            return render_template(home_path, card_list=card_list)
     
     return app
