45302c7d17459673a9acaaf9e5724f45719c3542
pctmoraes <paula.ct.moraes@gmail.com>
2023-10-27 14:21:18 -0300

    Code refactoring

--- app.py
+++ app.py
@@ -5,29 +5,35 @@ def create_app():
     app = Flask(__name__)
     app.debug = True
 
+    # Here would be the cards fetched from the DB
+    card_list = list()
+
     @app.route("/", methods=["GET", "POST"])
     def home():
-        card = Card('','','',0,False)
+        if request.method == "GET":
+            if card_list:
+                return render_template("home.html", card_list=card_list)
+            else:
+                return render_template("empty_wallet.html")
+        elif request.method == "POST":
+            form_class = request.form.get('form_class')
 
-        if request.method == "POST":
-            if request.form.get('card_name'):
-                card_ccv = int(request.form.get('card_ccv')) \
-                            if request.form.get('card_ccv') \
-                            and request.form.get('card_ccv').isnumeric() else 0
-                
-                is_card_default = True \
-                                    if request.form.get('is_card_default') else False
-                
+            if form_class == 'get_card_form':
+                return render_template("add_card.html", card=None)
+            else:
                 card = Card(
                     card_name = request.form.get('card_name'),
                     card_number = request.form.get('card_number'),
                     card_valid_thru = request.form.get('card_valid_thru'),
-                    card_ccv = card_ccv,
-                    is_card_default = is_card_default
+                    card_ccv = request.form.get('card_ccv'),
+                    is_card_default = request.form.get('is_card_default')
                 )
-            else:
-                card.card_name = 'show_form'
 
-        return render_template("home.html",card=card)
+                if card.validation_messages:
+                    return render_template("add_card.html", card=card)
+                else:
+                    card_list.append(card)
+                    print('\n\n', card_list)
+                    return render_template("home.html",card_list=card_list)
     
     return app
--- models/card.py
+++ models/card.py
@@ -7,12 +7,35 @@ class Card(object):
         card_valid_thru: str,
         card_ccv: int,
         is_card_default: bool
-    ):
+    ) -> None:
+        self.validation_messages = list()
+
+        if type(card_name) is not str:
+            self.validation_messages.append("O nome impresso no cartão não é válido.")
+        if len(card_name) == 0:
+            self.validation_messages.append("O nome impresso no cartão não pode ser vazio.")
+        
+        
+        if type(card_number) is not str:
+            self.validation_messages.append("O número do cartão não é válido.")
+        if len(card_number) == 0:
+            self.validation_messages.append("O número do cartão não pode ser vazio.")
+        
+        
+        if type(card_valid_thru) is not str:
+            self.validation_messages.append("A data de validade do cartão não é válida.")
+        if len(card_valid_thru) == 0:
+            self.validation_messages.append("A data de validade do cartão não pode ser vazia.")
+        
+        if card_ccv.isnumeric() == False:
+            self.validation_messages.append("O CVV do cartão não é válido.")
+            card_ccv = 0
+        
         self.card_name = card_name
         self.card_number = card_number
         self.card_valid_thru = card_valid_thru
-        self.card_ccv = card_ccv
-        self.is_card_default = is_card_default
+        self.card_ccv = int(card_ccv)
+        self.is_card_default = True if is_card_default else False
         self.card_brand = self.get_card_brand()
     
     def get_card_brand(self):
@@ -21,6 +44,4 @@ class Card(object):
         elif self.card_number[:2] == '12':
             return 'Visa'
         else:
-            return '' 
-
-            
+            return 'Bandeira desconhecida' 
