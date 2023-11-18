76b700fe26b8cad2b85c129e2970ef31e3122c10
pctmoraes <paula.ct.moraes@gmail.com>
2023-11-18 17:11:50 -0300

    Adds user authentication

--- pctmwallet/app.py
+++ pctmwallet/app.py
@@ -1,8 +1,12 @@
+import secrets
 from flask import Flask
 from routes import pages
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(pages)
+
+    # the key below was generated with secrets.token_urlsafe()
+    app.secret_key = 'fM-IwDXtvsHmppplTmgPZLyXTsEYqlEdtotbAquIiNE'
 
     return app
